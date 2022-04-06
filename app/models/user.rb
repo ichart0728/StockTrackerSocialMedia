ae8c66f2116ca7d64f6d5d4afc253c2f181040c3
ichikawaarata <maroon5.gingerale@gmail.com>
2022-04-06 22:40:18 +0900

    Self referentials association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -23,7 +25,7 @@ class User < ApplicationRecord
   end
 
   def full_name
-    return "#{first_name} #{last_name}" if first_name || last_name
+    return "#{first_name} #{last_name}" if first_name || last_namde
     "Anonymous"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
   root 'welcome#index'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'friends', to: 'users#friends'
   get 'search_stock', to: 'stocks#search'
 
 end
